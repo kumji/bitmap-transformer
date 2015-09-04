124d3f58815b558cc4e2d5a8ca4c5f09539deead
Kumji Park <kumji.p@gmail.com>
2015-09-04 14:49:40 -0700

    4th Sep 2:49pm

---
+++ test/gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
+
+
+gulp.task('jshint', function() {
+	return gulp.src('filecontrol.js')
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', function() {
+	return gulp.src('test/**/*test.js')
+	.pipe(mocha());
+});
+
+
+gulp.task('default', ['jshint', 'test']);
