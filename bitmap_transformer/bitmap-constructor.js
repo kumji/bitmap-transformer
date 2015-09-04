b41530830d63e7bf7e8804a6a33204a3eb4ca09b
clareluna <clarelunamonahan@gmail.com>
2015-09-04 11:21:08 -0700

    grayscale and color inversion methods, untested

--- bitmap_transformer/bitmap-constructor.js
+++ bitmap_transformer/bitmap-constructor.js
@@ -2,11 +2,11 @@
 
 var Bitmap = { function(data) {
 	this.bitmapFile = ???
-	this.headerOffset = this.bitmapFile.splice(0, 14);
-	this.headerDIB = this.bitmapFile.splice(14, 40);
-	this.width = this.bitmapFile.readUint32[18];
-	this.height = this.bitmapFile.readUint32[22];
-	this.pixelArrayStart = this.bitmapFile.readUint32[54];
+	this.headerOffset = this.bitmapFile.buf.splice(0, 14);
+	this.headerDIB = this.bitmapFile.buf.splice(14, 40);
+	this.width = this.bitmapFile.buf.readUint32[18];
+	this.height = this.bitmapFile.buf.readUint32[22];
+	this.pixelArrayStart = this.bitmapFile.buf.slice(54, this.header.dataOffset);
 	this.binaryData = [];
 	this.pixelatedColors = [];
 	};
@@ -14,15 +14,14 @@ var Bitmap = { function(data) {
 // generate a way of reading out the the binary data for where header, width, height
 Bitmap.prototype.binaryData = function(header, width, height) {
 	var binaryData = {};
-	this.binaryData.header = this.bitmapFile.readUint32[14];
-	this.binaryData.width = this.bitmapFile.readUint32[18];
-	this.binaryData.height = this.bitmapFile.readUint32[22];
-	this.binaryData.push(binaryData);
+	binaryData.header = this.bitmapFile.readUint32[14];
+	binaryData.width = this.bitmapFile.readUint32[18];
+	binaryData.height = this.bitmapFile.readUint32[22];
+	binaryData.push(binaryData);
 }
 
 Bitmap.prototype.pixelArrayStart = function() {
 	this.pixelArrayStart = this.bitmapFile.readUint32[54]; //accounting for 14 bits header and 40 bits DIB
-
 }; //should go to 70
 //generate splice to offset palleteTable to zero from where the header ends DIB
 
